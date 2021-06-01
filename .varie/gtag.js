b8dcd438dfa2b3ea2c61ef051997fbf8f502de74
buzzqw <azanzani@gmail.com>
2021-06-01 14:49:48 +0200

    Delete .varie directory

--- .varie/gtag.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-TC7RK3JDES"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-TC7RK3JDES');
-</script>
