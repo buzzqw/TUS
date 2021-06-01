01284ea9d7a43fce195f5a8ef7c98745f05b0ba9
azanzani <azanzani@gmail.com>
2021-06-01 14:28:50 +0200

    sync

--- varie/gittex.sh
+++ varie/gittex.sh
@@ -1,14 +1,16 @@
 export NO_AT_BRIDGE=1
 
 pandoc -s "/home/azanzani/TUS/DBS/DBS - Dungeon Bell System.tex" -o "/home/azanzani/TUS/DBS/DBS - Dungeon Bell System.docx"
-zip -1 "/home/azanzani/TUS/TUS-git.zip" "/home/azanzani/TUS/screen.odt" "/home/azanzani/TUS/bell-lulu6.png" "/home/azanzani/TUS/copertina.png" "/home/azanzani/TUS/esempi-magie.tex" "/home/azanzani/TUS/gittex.sh" "/home/azanzani/TUS/magia-alternativa.tex" "/home/azanzani/TUS/DBS-monstruos-chapter.tex" "/home/azanzani/TUS/README.md" "/home/azanzani/TUS/The Untitled Bell System.pdf" "/home/azanzani/TUS/The Untitled Bell System.tex" "/home/azanzani/TUS/TUS-schedav6.ods" "/home/azanzani/TUS/TUS-schedav6.pdf" "/home/azanzani/TUS/DBS - Dungeon Bell System.tex" "/home/azanzani/TUS/DBS-magia-alternativa.tex" "/home/azanzani/TUS/DBS-abilita-arma-armature.tex" "/home/azanzani/TUS/DBS-scheda.ods" "/home/azanzani/TUS/DBS - Dungeon Bell System.pdf" "/home/azanzani/TUS/DBS-scheda.pdf" "/home/azanzani/TUS/theoldmagic.tex" "/home/azanzani/TUS/theoldmagic.pdf"
-rsync -a "screen.odt" "TUS-git.zip" "/home/azanzani/TUS/bell-lulu6.png" "DBS-abilita-arma-armature.tex" "DBS-scheda.ods" "DBS-scheda.pdf" "DBS - Dungeon Bell System.tex" "DBS-magia-alternativa.tex" "The Untitled Bell System.tex" "The Untitled Bell System.pdf" "copertina.png" "The Untitled Bell System.docx" "TUS-schedav6.ods" "TUS-schedav6.pdf" "DBS-monstruos-chapter.tex" "magia-alternativa.tex" "/home/azanzani/TUS/esempi-magie.tex" "/home/azanzani/TUS/DBS-abilita-arma-armature.tex" "/home/azanzani/TUS/gittex.sh" "DBS - Dungeon Bell System.pdf" "theoldmagic.tex" "theoldmagic.pdf" $HOME"/Dropbox/Ad&ZZ/Pazfinder/"
+#zip -1 "/home/azanzani/TUS/TUS/TUS-git.zip" "/home/azanzani/TUS/screen.odt" "/home/azanzani/TUS/bell-lulu6.png" "/home/azanzani/TUS/copertina.png" "/home/azanzani/TUS/esempi-magie.tex" "/home/azanzani/TUS/gittex.sh" "/home/azanzani/TUS/magia-alternativa.tex" "/home/azanzani/TUS/DBS-monstruos-chapter.tex" "/home/azanzani/TUS/README.md" "/home/azanzani/TUS/The Untitled Bell System.pdf" "/home/azanzani/TUS/The Untitled Bell System.tex" "/home/azanzani/TUS/TUS-schedav6.ods" "/home/azanzani/TUS/TUS-schedav6.pdf" "/home/azanzani/TUS/DBS - Dungeon Bell System.tex" "/home/azanzani/TUS/DBS-magia-alternativa.tex" "/home/azanzani/TUS/DBS-abilita-arma-armature.tex" "/home/azanzani/TUS/DBS-scheda.ods" "/home/azanzani/TUS/DBS - Dungeon Bell System.pdf" "/home/azanzani/TUS/DBS-scheda.pdf" "/home/azanzani/TUS/theoldmagic.tex" "/home/azanzani/TUS/theoldmagic.pdf"
+
+#rsync -a "screen.odt" "TUS-git.zip" "/home/azanzani/TUS/bell-lulu6.png" "DBS-abilita-arma-armature.tex" "DBS-scheda.ods" "DBS-scheda.pdf" "DBS - Dungeon Bell System.tex" "DBS-magia-alternativa.tex" "The Untitled Bell System.tex" "The Untitled Bell System.pdf" "copertina.png" "The Untitled Bell System.docx" "TUS-schedav6.ods" "TUS-schedav6.pdf" "DBS-monstruos-chapter.tex" "magia-alternativa.tex" "/home/azanzani/TUS/esempi-magie.tex" "/home/azanzani/TUS/DBS-abilita-arma-armature.tex" "/home/azanzani/TUS/gittex.sh" "DBS - Dungeon Bell System.pdf" "theoldmagic.tex" "theoldmagic.pdf" $HOME"/Dropbox/Ad&ZZ/Pazfinder/"
 
 rsync -a $HOME"/Dropbox/Federica/Le avventure di una mezza Pixie.odt" /home/azanzani/TUS/
 rsync -a $HOME"/Dropbox/Federica/Nuove avventure di una fatina.odt" /home/azanzani/TUS/
 
 
 git config --global credential.helper cache
+git ls-files --others --exclude-from=.git/info/exclude
 git add "/home/azanzani/TUS/"
 git add "/home/azanzani/DBS/changelog.md"
 git add "/home/azanzani/DBS/CODE_OF_CONDUCT.md"
@@ -41,12 +43,12 @@ git push https://buzzqw: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@github.com/buzz
 #git push https://github-ci-token:ad61b7957469255fde008a4f1c59fff6e868e131@github.com/buzzqw/TUS.git
 
 
-rclone copy "DBS/DBS-abilita-arma-armature.tex" mega:
-rclone copy "DBS/DBS - Dungeon Bell System.tex" mega:
-rclone copy "DBS/DBS-magia-alternativa.tex" mega:
-rclone copy "DBS/DBS-monstruos-chapter.tex" mega:
-rclone copy "DBS/DBS-scheda.ods" mega:
-rclone copy "DBS/DBS - Dungeon Bell System.pdf" mega:
+rclone copy "/home/azanzani/TUS/DBS/DBS-abilita-arma-armature.tex" mega:
+rclone copy "/home/azanzani/TUS/DBS/DBS - Dungeon Bell System.tex" mega:
+rclone copy "/home/azanzani/TUS/DBS/DBS-magia-alternativa.tex" mega:
+rclone copy "/home/azanzani/TUS/DBS/DBS-monstruos-chapter.tex" mega:
+rclone copy "/home/azanzani/TUS/DBS/DBS-scheda.ods" mega:
+rclone copy "/home/azanzani/TUS/DBS/DBS - Dungeon Bell System.pdf" mega:
 
 rclone dedupe mega:
 
