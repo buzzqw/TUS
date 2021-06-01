ff88f029fce8a5b735000a4a3f8a1cee0f9a35ef
azanzani <azanzani@gmail.com>
2021-06-01 14:06:01 +0200

    sync

---
+++ varie/gtag.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TC7RK3JDES"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TC7RK3JDES');
+</script>
