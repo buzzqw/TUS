d1ccb03a1fa8a276ac143eef0defc71bcca52914
azanzani <azanzani@gmail.com>
2021-08-12 15:36:42 +0200

    aggiunto OBSS

---
+++ OBSS/caricagit-lavoro.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd /home/azanzani/TUS/
+git commit -am "salvataggio"
+git push https://buzzqw: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@github.com/buzzqw/TUS.git
+
+#git push https://github-ci-token:ad61b7957469255fde008a4f1c59fff6e868e131@github.com/buzzqw/TUS.git
