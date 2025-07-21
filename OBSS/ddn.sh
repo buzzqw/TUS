4be21d5c06447503a3e10c1bc0c6b21a1c084b0e
buzzqw <azanzani@gmail.com>
2025-07-21 14:11:58 +0200

    sync

--- OBSS/ddn.sh
+++ OBSS/ddn.sh
@@ -343,12 +343,12 @@ convert_markdown() {
     
     local converted=0
     
-    if Latex2MarkDown.sh "$FILE1"  >/dev/null 2>&1; then
+    if latex2markdown.sh "$FILE1"  >/dev/null 2>&1; then
         log_success "OBSSv2.md generato"
         ((converted++))
     fi
     
-    if Latex2MarkDown.sh "$FILE2" >/dev/null 2>&1; then
+    if latex2markdown.sh "$FILE2" >/dev/null 2>&1; then
         log_success "OBSSv2-eng.md generato"
         ((converted++))
     fi
